c053f209bbfa6b5e44474b613123d127b9580545
Tian Wei You <tianweiyou@192.168.0.107>
2016-09-19 23:17:28 +0800

    JW:redBook Search set

--- YuWa/YuWa/Contents/discover/RBBook/RBHome/SearchView/SerachDetail/RBHomeSearchDetailViewController.m
+++ YuWa/YuWa/Contents/discover/RBBook/RBHome/SearchView/SerachDetail/RBHomeSearchDetailViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "RBHomeSearchDetailViewController.h"
+#import "RBHomeSearchToolsView.h"
 
 @interface RBHomeSearchDetailViewController ()
+@property (nonatomic,strong)RBHomeSearchToolsView * searchView;
 
 @end
 
@@ -16,8 +18,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self makeNavi];
 }
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.searchView.height = 30.f;
+    self.searchView.width = kScreen_Width - 40.f;
+}
+
+- (void)makeNavi{
+    self.searchView = [[[NSBundle mainBundle]loadNibNamed:@"RBHomeSearchToolsView" owner:nil options:nil]firstObject];
+    [self.searchView.textField setUserInteractionEnabled:NO];
+    WEAKSELF;
+    self.searchView.typeChooseBlock = ^(NSInteger type){
+        weakSelf.type = type;
+    };
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapAction)];
+    [self.searchView addGestureRecognizer:tap];
+    self.navigationItem.titleView = self.searchView;
+}
+
+- (void)tapAction{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 
 @end
