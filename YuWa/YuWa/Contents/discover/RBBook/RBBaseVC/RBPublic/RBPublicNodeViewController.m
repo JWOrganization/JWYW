74867ae14896c1de32113634601fc0c6c835aa70
Tian Wei You <tianweiyou@192.168.0.107>
2016-10-12 17:07:16 +0800

    JW:iOS9 image Add to Aldum

--- YuWa/YuWa/Contents/discover/RBBook/RBBaseVC/RBPublic/RBPublicNodeViewController.m
+++ YuWa/YuWa/Contents/discover/RBBook/RBBaseVC/RBPublic/RBPublicNodeViewController.m
@@ -10,6 +10,9 @@
 #import "RBPublicTagEditorViewController.h"
 
 #import "RBPublicToolView.h"
+#ifdef __IPHONE_9_0
+#import <Photos/Photos.h>
+#endif
 #import <AssetsLibrary/AssetsLibrary.h>
 #import "XHTagView.h"
 
@@ -25,6 +28,10 @@
 @property (nonatomic,assign)BOOL isChanged;
 
 @property (nonatomic,strong)ALAssetsLibrary *library;
+
+#ifdef __IPHONE_9_0
+@property (nonatomic,strong)PHPhotoLibrary *photoLibrary;
+#endif
 
 @end
 
@@ -237,7 +244,6 @@
     NSMutableArray * dataArr = self.tagSaveArr[self.imagePage];
     tagView.branchTexts = [NSMutableArray arrayWithArray:tagArr];
     tagView.tag = 1009;
-//    tagView.tagAnimationStyle = XHTagAnimationStyleAllRight;
     tagView.centerLocationPoint = point;
     tagView.tagAnimationStyle = animationStyle;
     [dataArr addObject:tagView];
@@ -302,16 +308,27 @@
 
 #pragma mark - Save Image To My Album
 - (void)saveImgToAlbumWithImgIdx:(NSInteger)idx{
+    WEAKSELF;
+#ifdef __IPHONE_9_0
+    [self saveImageToCustomAblum:self.imagesArr[idx]];
+    return;
+#endif
+    
     if (!self.library) {//创建相册
         self.library = [[ALAssetsLibrary alloc] init];
         [self.library addAssetsGroupAlbumWithName:@"雨娃" resultBlock:^(ALAssetsGroup *group) {
             //创建相簿成功
+            [weakSelf saveToAlbumWithMetadata:@{} imageData:UIImagePNGRepresentation(weakSelf.imagesArr[idx]) customAlbumName:@"雨娃" completionBlock:nil failureBlock:nil];
         } failureBlock:^(NSError *error) {
             UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"存储失败" message:@"请打开 设置-隐私-照片 来进行设置" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
             [alert show];
         }];
+    }else{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self saveToAlbumWithMetadata:@{} imageData:UIImagePNGRepresentation(self.imagesArr[idx]) customAlbumName:@"雨娃" completionBlock:nil failureBlock:nil];
+        });
     }
-    [self saveToAlbumWithMetadata:@{} imageData:UIImagePNGRepresentation(self.imagesArr[idx]) customAlbumName:@"雨娃" completionBlock:nil failureBlock:nil];
+    
 }
 
 - (void)saveToAlbumWithMetadata:(NSDictionary *)metadata imageData:(NSData *)imageData customAlbumName:(NSString *)customAlbumName completionBlock:(void (^)(void))completionBlock failureBlock:(void (^)(NSError *error))failureBlock{
@@ -365,5 +382,71 @@
     }];
 }
 
+#pragma mark - iOS9 SaveImage To Aldum
+
+#ifdef __IPHONE_9_0
+/**同步方式保存图片到系统的相机胶卷中---返回的是当前保存成功后相册图片对象集合*/
+- (PHFetchResult<PHAsset *> *)syncSaveImageWithPhotosWithImage:(UIImage *)photo{//--1 创建 ID 这个参数可以获取到图片保存后的 asset对象
+    __block NSString *createdAssetID = nil;
+    
+    //--2 保存图片
+    NSError *error = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{//----block 执行的时候还没有保存成功--获取占位图片的 id，通过 id 获取图片---同步
+        createdAssetID = [PHAssetChangeRequest             creationRequestForAssetFromImage:photo].placeholderForCreatedAsset.localIdentifier;
+    } error:&error];
+    
+    //--3 如果失败，则返回空
+    if (error)return nil;
+    
+    //--4 成功后，返回对象//获取保存到系统相册成功后的 asset 对象集合，并返回
+    PHFetchResult<PHAsset *> *assets = [PHAsset fetchAssetsWithLocalIdentifiers:@[createdAssetID] options:nil];
+    return assets;
+    
+}
+/**拥有与 APP 同名的自定义相册--如果没有则创建*/
+-(PHAssetCollection *)getAssetCollectionWithAppNameAndCreateIfNo{//1 获取以 APP 的名称
+    NSString *title = @"雨娃";
+    //2 获取与 APP 同名的自定义相册
+    PHFetchResult<PHAssetCollection *> *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *collection in collections) {//遍历
+        if ([collection.localizedTitle isEqualToString:title]) return collection;//找到了同名的自定义相册--返回
+    }
+    
+    //说明没有找到，需要创建
+    NSError *error = nil;
+    __block NSString *createID = nil; //用来获取创建好的相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{//发起了创建新相册的请求，并拿到ID，当前并没有创建成功，待创建成功后，通过 ID 来获取创建好的自定义相册
+        PHAssetCollectionChangeRequest *request = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:title];
+        createID = request.placeholderForCreatedAssetCollection.localIdentifier;
+    } error:&error];
+    if (error) {
+        return nil;
+    }else{//通过 ID 获取创建完成的相册 -- 是一个数组
+        return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[createID] options:nil].firstObject;
+    }
+    
+}
+/**将图片保存到自定义相册中*/
+- (void)saveImageToCustomAblum:(UIImage *)photo{//1 将图片保存到系统的【相机胶卷】中---调用刚才的方法
+    PHFetchResult<PHAsset *> *assets = [self syncSaveImageWithPhotosWithImage:photo];
+    if (assets == nil){
+        MyLog(@"保存失败");
+        return;
+    }
+    
+    //2 拥有自定义相册（与 APP 同名，如果没有则创建）--调用刚才的方法
+    PHAssetCollection *assetCollection = [self getAssetCollectionWithAppNameAndCreateIfNo];
+    if (assetCollection == nil)return; //相册创建失败
+    
+    //3 将刚才保存到相机胶卷的图片添加到自定义相册中 --- 保存带自定义相册--属于增的操作，需要在PHPhotoLibrary的block中进行
+    NSError *error = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{//--告诉系统，要操作哪个相册
+        PHAssetCollectionChangeRequest *collectionChangeRequest = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:assetCollection];
+        [collectionChangeRequest insertAssets:assets atIndexes:[NSIndexSet indexSetWithIndex:0]];
+    } error:&error];
+    
+    MyLog(@"保存%@",error?@"失败":@"成功");
+}
+#endif
 
 @end
