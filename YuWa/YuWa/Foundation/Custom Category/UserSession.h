407e8693f016a2738bbb12d1e63be833c20e7f53
Tian Wei You <tianweiyou@192.168.0.105>
2016-10-19 09:52:33 +0800

    JW:UserSession set

--- YuWa/YuWa/Foundation/Custom Category/UserSession.h
+++ YuWa/YuWa/Foundation/Custom Category/UserSession.h
@@ -15,20 +15,27 @@
 @property (nonatomic,copy)NSString * account;  //账户
 @property (nonatomic,copy)NSString * password;   //密码
 @property (nonatomic,copy)NSString * hxPassword;   //环信密码
-@property(nonatomic,strong)NSString*rand;
-@property(nonatomic,strong)NSString*qq;     //QQ
-@property(nonatomic,strong)NSString*areaid;   //区id
-@property(nonatomic,strong)NSString*statu;     //状态
-@property(nonatomic,strong)NSString*currency; //货币
-@property(nonatomic,strong)NSString*personality;   //个人签名
-
-@property (nonatomic,copy)NSString * aldumCount;  //专辑个数
 @property (nonatomic,copy)NSString * inviteID;  //邀请ID
 
+@property(nonatomic,copy)NSString * logo;//头像
+@property(nonatomic,copy)NSString * nickName;//昵称
+@property(nonatomic,copy)NSString * sex;//性别
+@property(nonatomic,copy)NSString * birthDay;//生日
+@property(nonatomic,copy)NSString * local;   //常驻地
+@property(nonatomic,copy)NSString * personality;   //个人签名
+//@property(nonatomic,copy)NSString * statu;     //状态
+//@property(nonatomic,copy)NSString * currency; //货币
+
+@property(nonatomic,copy)NSString * attentionCount;//关注数
+@property(nonatomic,copy)NSString * fans;//粉丝
+@property(nonatomic,copy)NSString * praised;//被赞数
+@property(nonatomic,copy)NSString * collected;//被收藏
+@property (nonatomic,copy)NSString * aldumCount;  //专辑个数
+
+@property (nonatomic,assign)BOOL isVIP;//是否是会员
 
 //已经登录
 @property(nonatomic,assign)BOOL isLogin;   //是否登录
-
 
 +(UserSession*)instance;  //创建单例
 +(void)clearUser;   //退出登录 删除数据
